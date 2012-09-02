1564d78d47692ff12608a62237b370f87766ceb7
c9s <cornelius.howl@gmail.com>
2012-09-02 12:15:23 +0800

    Add writeSection and writeStashes method

--- src/ConfigKit/ConfigLoader.php
+++ src/ConfigKit/ConfigLoader.php
@@ -6,20 +6,44 @@ class ConfigLoader
 {
     public $stashes = array();
 
+    public $files = array();
+
     public function load($section,$file)
     {
+        // register to files
+        $this->files[ $section ] = array($file);
         return $this->stashes[ $section ] = ConfigCompiler::load($file);
     }
 
     public function merge($section,$file)
     {
         if( isset($this->stashes[$section]) ) {
+            $this->files[ $section ][] = $file;
             $config = ConfigCompiler::load($file);
-            $this->stashes[$section] = array_merge( $this->stashes[$section] , $config );
+            return $this->stashes[$section] = array_merge( $this->stashes[$section] , $config );
         } else {
             return $this->load($section,$file);
         }
     }
+
+
+    /**
+     * write section config to a file.
+     */
+    public function writeSection($section,$file)
+    {
+        if( ! isset($this->stashes[$section]) ) {
+            throw new Exception("$section section is not loaded.");
+        }
+        $config = $this->stashes[$section];
+        return ConfigCompiler::write_config($file,$config);
+    }
+
+    public function writeStashes($file)
+    {
+        return ConfigCompiler::write_config($file,$this->stashes);
+    }
+
 
     /**
      * Allow more useful getter
