519cbbb66ac8845b76ec1d03513677d0acc806ed
c9s <yoanlin93@gmail.com>
2014-02-21 22:00:11 +0800

    More accessor tests

--- tests/ConfigKit/AccessorTest.php
+++ tests/ConfigKit/AccessorTest.php
@@ -34,5 +34,19 @@ class AccessorTest extends PHPUnit_Framework_TestCase
         is( 1, $config->lookup('Product.zoom_image') );
         is( null, $config->lookup('Product.zoom_image.width') );
     }
+
+    /**
+     * @depends testConfigConstructor
+     */
+    public function testReturnAccessor($config)
+    {
+        $accessor = $config->lookup('Product.image');
+        ok($accessor);
+        class_ok('ConfigKit\\Accessor', $accessor);
+        is( 100,  $accessor['width'] );
+        is( 200,  $accessor['height'] );
+        is( 100,  $accessor->width );
+        is( 200,  $accessor->height );
+    }
 }
 
