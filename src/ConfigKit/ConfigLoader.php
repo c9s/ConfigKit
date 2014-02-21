aeb1561388a4293c76193d624e234ff3b5aca6c7
c9s <yoanlin93@gmail.com>
2014-02-21 22:36:59 +0800

    reuse lookup method from Accessor

--- src/ConfigKit/ConfigLoader.php
+++ src/ConfigKit/ConfigLoader.php
@@ -110,46 +110,11 @@ class ConfigLoader
      */
     public function get($section, $key = null)
     {
-        /*
-        if( isset( $this->getterCache[ $key ] ) ) 
-            return $this->getterCache[ $key ];
-         */
-        $config = $this->getSection( $section );
-        if( $key == null ) 
-        {
-            if( ! empty($config) )
-                return new Accessor($config);
-            return null;
+        $config = new Accessor($this->getSection( $section ));
+        if ( $key ) {
+            return $config->lookup( $key );
         }
-
-        if( isset($config[ $key ]) ) 
-        {
-            if( is_array( $config[ $key ] ) ) {
-                if( empty($config[ $key ]) )
-                    return null;
-                return new Accessor($config[ $key ]);
-            }
-            return $config[ $key ];
-        }
-
-        if( false !== strchr( $key , '.' ) ) 
-        {
-            $parts = explode( '.' , $key );
-            $ref = $config;
-            while( $ref_key = array_shift( $parts ) ) {
-                if( ! isset($ref[ $ref_key ]) )
-                    return null;
-                $ref = & $ref[ $ref_key ];
-            }
-
-            if( is_array( $ref ) ) {
-                if( empty($ref) )
-                    return null;
-                return new Accessor($ref);
-            }
-            return $ref;
-        }
-        return null;
+        return $config;
     }
 
     public function isLoaded($sectionId) {
